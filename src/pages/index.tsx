bc190bf8d0407a170bc4ca8a7be8cd8805ba76aa
PercyTuncar <percy.edgar.tuncar@gmail.com>
2020-10-15 23:11:51 -0500

    home edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import Testimonials from 'components/Testimonials';
 const IndexPage: React.FC = () => {
   return (
     <Layout>
-      <SEO title="About Me" />
+      <SEO title="Sobre mi" />
       <HeroBanner />
       <Services />
       <hr />
